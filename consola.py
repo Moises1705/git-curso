b7f0ce20c73b524ae8698640be1d5091b26002f8
Fazt <contact@faztweb.com>
2025-05-25 20:34:43 -0500

    ahora el archivo tiene una condicional

--- consola.py
+++ consola.py
@@ -1,1 +1,8 @@
 print("Probando git")
+
+valor = int(input("Elige 1 o 2: "))
+
+if valor == 1 or valor == 2:
+    print(f"Usted a elegido {valor}.")
+else:
+    print("Usted no ha elegido ningún valor.")
